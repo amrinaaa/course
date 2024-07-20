0c7d72030fd0ef673e0fef5b9208118a5fa5da8e
amrina rossada <amrina0612@gmail.com>
2024-07-21 00:54:49 +0800

    update

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,8 +1,35 @@
-import React from "react"
+import React from "react";
 import {Formik, Form, Field, ErrorMessage} from "formik";
 
 function CreatePost() {
-    return <div>CreatePost</div>;
+    return (
+    <div className="createPostPage">
+        <Formik> 
+            <Form>
+                <label>Title : </label>
+                <Field 
+                autocomplete= "off"
+                id="inputCreatePost" 
+                name="title" 
+                placeholder="Title"/>
+
+                <label>Post : </label>
+                <Field
+                autocomplete= "off"
+                id="inputCreatePost" 
+                name="postText" 
+                placeholder="Create Post"/>
+
+                <label>Username : </label>
+                <Field 
+                autocomplete= "off"
+                id="username" 
+                name="username" 
+                placeholder="Username"/>
+            </Form>
+            </Formik>
+    </div>
+    );
 }
 
 export default CreatePost;
