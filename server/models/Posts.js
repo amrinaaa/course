01b99ae420622ee58473b29418e6d50eb5d2c805
amrina rossada <amrina0612@gmail.com>
2024-07-27 01:28:40 +0800

    update

--- server/models/Posts.js
+++ server/models/Posts.js
@@ -1,25 +1,23 @@
-const { hasMany } = require("sequelize");
-
 module.exports = (sequelize, DataTypes) => {
     const Posts = sequelize.define("Posts", {
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        postText: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      postText: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     });
-
-    Posts.associate = (models) => { 
-        Posts.hasMany(models.Comments, {
-            onDelete: "cascade",
-        });
+  
+    Posts.associate = (models) => {
+      Posts.hasMany(models.Comments, {
+        onDelete: "cascade",
+      });
     };
     return Posts;
-};
+  };
--- server/routes/Comments.js
+++ server/routes/Comments.js
@@ -2,18 +2,16 @@ const express = require("express");
 const router = express.Router();
 const { Comments } = require("../models");
 
-// Rute untuk mendapatkan komentar berdasarkan PostId
 router.get("/:postId", async (req, res) => {
-    const postId = req.params.postId;
-    const comments = await Comments.findAll({ where: { PostId: postId } });
-    res.json(comments);
+  const postId = req.params.postId;
+  const comments = await Comments.findAll({ where: { PostId: postId } });
+  res.json(comments);
 });
 
-// Rute untuk membuat komentar
 router.post("/", async (req, res) => {
-    const comment = req.body;
-    await Comments.create(comment);
-    res.json(comment);
+  const comment = req.body;
+  await Comments.create(comment);
+  res.json(comment);
 });
 
 module.exports = router;
