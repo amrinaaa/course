17802f53befd6ae9cb33930d52e85eb1c803cf15
amrina rossada <amrina0612@gmail.com>
2024-07-27 02:05:43 +0800

    update

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -5,23 +5,59 @@ import axios from "axios";
 function Post() {
   let { id } = useParams();
   const [postObject, setPostObject] = useState({});
+  const [comments, setComments] = useState([]);
+  const [newComment, setNewComment] = useState("");
 
   useEffect(() => {
+    // Fetch post data
     axios.get(`http://localhost:3001/posts/byId/${id}`).then((response) => {
       setPostObject(response.data);
     });
+
+    // Fetch comments data
+    axios.get(`http://localhost:3001/comments/${id}`).then((response) => {
+      setComments(response.data);
+    });
   }, [id]);
+
+  const handleAddComment = () => {
+    axios.post("http://localhost:3001/comments", {
+      commentBody: newComment,
+      PostId: id
+    }).then(response => {
+      setComments([...comments, response.data]); // Add new comment to state
+      setNewComment(""); // Clear input field
+    }).catch(error => {
+      console.error("Error adding comment:", error);
+    });
+  };
 
   return (
     <div className="postPage">
       <div className="leftSide">
         <div className="post" id="individual">
-          <div className="title"> {postObject.title} </div>
+          <div className="title">{postObject.title}</div>
           <div className="body">{postObject.postText}</div>
           <div className="footer">{postObject.username}</div>
         </div>
       </div>
-      <div className="rightSide">Comment Section</div>
+      <div className="rightSide">
+        <div className="addCommentContainer">
+          <input
+            type="text"
+            placeholder="Comment..."
+            autoComplete="off"
+            value={newComment}
+            onChange={(e) => setNewComment(e.target.value)}
+          />
+          <button onClick={handleAddComment}>Add Comment</button>
+        </div>
+        <div className="ListOfComments">
+          {comments.map((comment, key) => (
+            <div key={key} className="comment">{comment.commentBody}</div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
