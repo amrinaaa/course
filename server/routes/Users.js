cabd7c4d52952f9f2c2ad64129a26580ea5f255c
amrina rossada <amrina0612@gmail.com>
2024-07-29 00:03:33 +0800

    log

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { Users } = require("../models");
 const bcrypt = require("bcrypt");
+const { where } = require("sequelize");
 
 router.post("/", async (req, res) => {
     const {username, password } = req.body;
@@ -12,6 +13,20 @@ router.post("/", async (req, res) => {
             });
             res.json("Success");
     });
+    router.post("/", async (req, res) => {
+        const {username, password } = req.body;
+        
+        const user = await Users.findOne({where: {username: username}});
+
+        if  (!user) res.json({error: "User tidak ditemukan"});
+
+        bcrypt.compare(password, user.password).then((match) => {
+            if (!match) res.json({error: "Periksa kembali username dan password"});
+
+            res.json("Berhasil Login");
+        });
+        });
+
 });
 
 module.exports = router;
